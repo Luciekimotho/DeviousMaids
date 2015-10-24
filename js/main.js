2fd01a2b33939a9e01771afce795ce018fe7faba
Luciekimotho <luciekimotho@gmail.com>
2015-10-24 21:30:29 +0300

    Restarting the project

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('a.read_more').click(function(event){
-        event.preventDefault();
-        $this.parents('.synopsis').find('.more_text').show();
-    });
-          });
-$("#carousel").carousel();
